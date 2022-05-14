325994cf6a24df7bd0a2c2e816802a721e5b9caf
Manith Mam <manith7.mam@gmail.com>
2022-05-14 19:57:15 +0200

    Add useItem, inspectItem, pickUpItem method and inventory

--- src/main/java/classes/Player.java
+++ src/main/java/classes/Player.java
@@ -1,12 +1,17 @@
 package classes;
 
+import java.util.HashSet;
+
 public class Player {
     private Room currentRoom;
-
-    public Player(Room currentRoom){
+    private final HashSet<Items> inventory;
+    public Player(Room currentRoom, HashSet<Items> inventory){
         this.currentRoom = currentRoom;
+        this.inventory = inventory;
     }
 
+    //TODO: Room could be accessible or inaccessible
+    // -> Accessibility not yet implemented
     private void changeRoom(Room newRoom){
         this.currentRoom = newRoom;
     }
@@ -14,4 +19,19 @@ public class Player {
     private void inspectRoom(){
         System.out.println(this.currentRoom.getDescription());
     }
+
+    private void inspectItem(int itemId){
+        Items.getDesc(itemId);
+    }
+
+    private void pickUpItem(Items item){
+        inventory.add(item);
+    }
+
+    //TODO: Room accessibility not yet implemented
+    private void useItem(Room lockedRoom, Items item){
+
+    }
+
+
 }
