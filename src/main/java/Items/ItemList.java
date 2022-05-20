3f0eae7a4a3b95964536eba3f487ffca81ff3974
ru006 <umic.senolgmail.com>
2022-05-20 13:24:38 +0200

    -Updated Item ID distribution

--- src/main/java/Items/ItemList.java
+++ src/main/java/Items/ItemList.java
@@ -1,7 +1,5 @@
 package Items;
 
-import Items.Items;
-
 public class ItemList extends Items {
 
     //place to construct new items, to be stored in itemList<Items>
@@ -9,6 +7,6 @@ public class ItemList extends Items {
     //test for different construction methods
     Items baseball_bat = new Items();
 
-    Items rope = new Items(2, "Rope", true, "A strong rope that could be useful later on");
+    Items rope = new Items("Rope", true, "A strong rope that could be useful later on");
 
 }
