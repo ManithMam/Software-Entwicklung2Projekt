00cf385ef205f960aff3858a8d81c94d33ad3a3e
Manith Mam <manith7.mam@gmail.com>
2022-06-07 11:51:21 +0200

    Configure log4j2.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module SE2StartupProject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
 
     opens mainpackage;
     opens classes;
