f834f054cdc9557388249759d380ac823a780491
Manith <mm334@hdm-stuttgart.de>
2022-06-29 16:24:00 +0200

    Revert "Remove error screen"
    This reverts commit 077f8168e0b384e7376af7123c8e75fbf1bd36c7.

---
+++ src/main/java/de/stuttgart_hdm/mi/se2/gui/controller/ErrorController.java
@@ -0,0 +1,22 @@
+package de.stuttgart_hdm.mi.se2.gui.controller;
+
+import de.stuttgart_hdm.mi.se2.gui.view.GameView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ErrorController {
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private void initialize() {
+        exitBtn.setOnAction(this::exit);
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+        GameView.getPrimaryStage().close();
+    }
+}
